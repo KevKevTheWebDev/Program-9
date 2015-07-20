081e580394778d1f553cc060fc33d089ba7f3536
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-19 21:21:24 -0400

    Update IfStatement.java

--- IfStatement.java
+++ IfStatement.java
@@ -1,1 +1,14 @@
-# Program-9
+import java.util.Scanner;
+
+class apples{
+	public static void main(String args[]){
+		int test=6;
+		
+		if (test==9){
+		System.out.println("Equal");
+		}
+		else{
+			System.out.println("Not Equal");
+		}
+	}
+}
